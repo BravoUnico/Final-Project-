5f8e96d6353832cf40a7a57633fb17fa3c8eb5e1
Alejandro Sanchez <sancha0701@gmail.com>
2024-01-26 01:48:53 -0500

    getting fetching data api testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
 
 
 function App() {
- 
+ const [search, setSearch] = useState('')
+ const [ values, setValues] = useState('')
+ const [ icon, setIcon] = useState('')
+ const [ weather, setWeather] = useState('')
 
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=rome&lang=en&units=imperial&appid=d9efd9a630e65411cc199912a1fd62e4`
+  const getData = async () => {
+   const data = await axios.get(URL)
+   console.log(data)
+   setWeather(data.data)
+
+  }
+  useEffect(() => {
+    getData()
+  },[])
   return (
     <>
-      <div>
-       <h1>Final Project</h1>
+      <div className="App">
+       <h1>Weather App</h1>
+       <h2>Final Project</h2>
       </div>
       
     </>
