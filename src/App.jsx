42fac1657beb3e5aebc96d8b22f2eebfe909fdaa
Alejandro Sanchez <sancha0701@gmail.com>
2024-01-28 23:06:33 -0500

    missing dependency in useEffect don/t know how to fix it, I use now await fetch but I get error another error export App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,73 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
-
+import Icons from './components/Icons.jsx'
 
 function App() {
- const [search, setSearch] = useState('')
+ const [search, setSearch] = useState('search')
  const [ values, setValues] = useState('')
  const [ icon, setIcon] = useState('')
- const [ weather, setWeather] = useState('')
-
+ 
+// fetching data from API
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=rome&lang=en&units=imperial&appid=d9efd9a630e65411cc199912a1fd62e4`
+  // const getData = async () => {
+  //  const data = await axios.get(URL)
   const getData = async () => {
-   const data = await axios.get(URL)
-   console.log(data)
-   setWeather(data.data)
+    await fetch(URL)
+    .then(response => {return response.json()})
+    .then(data => {
+   if(data.cod >= 400) {
+    setValues(false)
+   }
+   else{
+    console.log(data);
+    setValues(data)
+    setIcon(data.weather[0].main) // will show icon according to weather
+   }
+  })
+  .catch(error => {
+    console.log(error)
+  })
 
+  const handleSearch = (e) =>{
+    console.log(e.target.values)
+    setSearch(e.target.values)
   }
   useEffect(() => {
-    getData()
-  },[])
-  return (
+   getData() 
+  },[search]);
+      return (
     <>
-      <div className="App">
+      <div className="App-container">
        <h1>Weather App</h1>
        <h2>Final Project</h2>
+       <div className="row">
+        <input 
+        onKeyDown={handleSearch}
+        type="text"
+        autoFocus
+        />
+       </div>
+      </div>
+      <div className="card">
+        {(values) ? (
+          <div className="card-container">
+            <h1 className="city-name">{values.name}</h1>
+
+            <p className="temp">{values.main.temp.toFixed(0)}&deg;</p>
+            <img className="icon" src={Icons(icon)} alt="icon-weather" />
+            {/* footer */}
+            <div className="card-footer">
+              <p className="temp-max-min">{values.main.temp_min.toFixed(0)}&deg; | {values.main.temp_max.toFixed(0)}&deg;</p>
+              </div>
+          </div>
+        ) : (
+          <h1>{"city not found"}</h1>
+        ) } 
       </div>
       
     </>
-  )
-}
+  );
+  }
 
-export default App
+  export default App
+  
---
+++ src/components/Icons.jsx
@@ -0,0 +1,45 @@
+
+const Icon = (icon) => {
+    switch (icon) {
+        case 'Thunderstorm':
+            icon='public/icons/thunderstorms.svg'
+            console.log("thunderstorm")
+            break;
+        case 'Snow':
+            icon='public/icons/snow.svg'
+            console.log("snow")
+            break;
+        case 'Rain':
+            icon='public/icons/rain.svg'
+            console.log("snow")
+            break;
+        case 'Haze':
+            icon='public/icons/rain.svg'
+            console.log("haze");
+            break;
+        case 'Fog':
+            icon='public/icons/fog.svg'
+            break;
+        case 'Cloudy':
+            icon='public/icons/cloudy.svg'
+            break;
+        case 'Clear-night':
+            icon='public/icons/clear-night.svg'
+            break;
+            
+    
+        default:
+            icon='public/icons/clear-day.svg'
+            console.log("clear");
+    }
+    
+    
+    
+    
+    return icon (
+        
+    );
+
+}
+
+export default Icon
